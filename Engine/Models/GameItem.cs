63f411ebbe25d89e54a5049a1dac850ffbeea912
Jtaim <jtaimneas@gmail.com>
2020-04-30 17:12:15 -0400

    Grouping GameItems in Inventories

--- Engine/Models/GameItem.cs
+++ Engine/Models/GameItem.cs
@@ -5,14 +5,16 @@
         public int ItemTypeID { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
+        public bool IsUnique { get; set; }
 
-        public GameItem(int itemTypeID, string name, int price)
+        public GameItem(int itemTypeID, string name, int price, bool isUnique = false)
         {
             ItemTypeID = itemTypeID;
             Name = name;
             Price = price;
+            IsUnique = isUnique;
         }
 
-        public GameItem Clone() => new GameItem(ItemTypeID, Name, Price);
+        public GameItem Clone() => new GameItem(ItemTypeID, Name, Price, IsUnique);
     }
 }

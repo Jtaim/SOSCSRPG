7faf9ebe40311a187e7a13d0446159543c1de2d5
Jtaim <jtaimneas@gmail.com>
2020-05-07 11:33:31 -0400

    minor fix in the TestCombatService.cs

--- TestEngine/Services/TestCombatService.cs
+++ TestEngine/Services/TestCombatService.cs
@@ -11,8 +11,8 @@ namespace TestEngine.Services
         public void Test_FirstAttacker()
         {
             // Player and monster with dexterity 12
-            var player = new Player("", "", 0, 0, 0, 18, 0);
-            var monster = new Monster(0, "", "", 0, 12, null, 0, 0);
+            var player = new Player("", "", 0, 12, 0, 18, 0);
+            var monster = new Monster(0, "", "", 12, 0, null, 0, 0);
 
             var result = CombatService.FirstAttacker(player, monster);
         }
